2bf9854d15279c169c390773f9b49bddca2779f3
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-11-13 17:23:47 +0700

    A

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,14 +7,14 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{ public_path('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ public_path('assets/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ public_path('assets/css/AdminLTE.min.css') }}">
-    <link rel="stylesheet" href="{{ public_path('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/AdminLTE.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{ public_path('assets/css/_all-skins.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/_all-skins.min.css') }}">
     <!-- Google Font -->
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
@@ -22,21 +22,20 @@
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
-<?php require_once 'header.php';
-?>
+@include('layouts.header')
 
 <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Main content -->
         <section class="content">
             <!--            Nội dung hiển thị ở đây-->
-            <?php echo $this->content; ?>
+            @yield('content')
         </section>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
 
-<?php require_once 'footer.php'; ?>
+@include('layouts.footer')
 <!-- Add the sidebar's background. This div must be placed
          immediately after the control sidebar -->
 
@@ -44,16 +43,16 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="{{ public_path('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ public_path('assets/js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-ui.min.js') }}"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="{{ public_path('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ public_path('assets/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('assets/js/adminlte.min.js') }}"></script>
 <!--CKEditor -->
-<script src="{{ public_path('assets/ckeditor/ckeditor.js') }}"></script>
+<script src="{{ asset('assets/ckeditor/ckeditor.js') }}"></script>
 <!--My SCRIPT-->
-<script src="{{ public_path('assets/js/script.js') }}"></script>
+<script src="{{ asset('assets/js/script.js') }}"></script>
 </body>
 </html>
